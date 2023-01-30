d511ab6bbd5aef51749966a00d4b78fb1ce2a79d
mmardiansyah17 <mardiansyahm002@gmail.com>
2023-01-30 20:33:19 +0700

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,60 @@
 import React from "react";
 import { useState } from "react";
-import MyDropzone from "./components/Dropzone";
-import axios from "axios";
-import FileSaver from "file-saver";
 import ImageSection from "./components/ImageSection";
+import MyDropzone from "./components/MyDropzone";
 export default function App() {
-  const [files, setFiles] = useState([]);
-  const [resultPath, setResultPath] = useState();
-  const submitHandler = async () => {
-    try {
-      const formData = new FormData();
-
-      files.forEach((file, index) => {
-        formData.append(`file`, file);
-      });
-      axios
-        .post("http://localhost:3000/upload", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .then((res) => {
-          setResultPath(res.data.pathFile);
-        });
-    } catch (error) {
-      console.log("woui", error);
+  const [images, setImages] = useState([
+    // "../../public/img/1.png",
+    // "../../public/img/2.png",
+    // "../../public/img/3.png",
+  ]);
+  function upHandler(id) {
+    if (id + 1 !== images.length) {
+      const result = [...images];
+      let currentImage = images[id];
+      result[id] = result[id + 1];
+      result[id + 1] = currentImage;
+      setImages(result);
     }
-  };
+  }
+  function downHandler(id) {
+    if (id !== 0) {
+      const result = [...images];
+      let currentImage = images[id];
+      result[id] = result[id - 1];
+      result[id - 1] = currentImage;
+      console.log(result);
+      setImages(result);
+    }
+  }
+
+  function onDrop(file) {
+    setImages(file);
+  }
 
   return (
-    <div className="p-2">
-      {/* <h1 className="text-center font-semibold text-xl">Gambar ke Pdf</h1>
-      <MyDropzone /> */}
-      <ImageSection />
+    <div className="p-2  ">
+      <h1 className="text-center font-semibold text-xl">Gambar ke Pdf</h1>
+      {/* tampilkan hasil gambar jika ada */}
+      {images.length !== 0 ? (
+        <>
+          {images.map((data, idx) => {
+            return (
+              <ImageSection
+                id={idx}
+                key={`image.${idx}`}
+                upHandler={upHandler}
+                downHandler={downHandler}
+                img={URL.createObjectURL(data)}
+              />
+            );
+          })}
+        </>
+      ) : (
+        <>
+          <MyDropzone onDrop={onDrop} />
+        </>
+      )}
     </div>
   );
 }
--- client/src/components/ImageSection.jsx
+++ client/src/components/ImageSection.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
-export default function ImageSection() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown, faCaretUp, faCoffee, faTrash } from "@fortawesome/free-solid-svg-icons";
+export default function ImageSection({ img, upHandler, downHandler, id }) {
   return (
-    <div>
-      <img src="../../public/img/Screenshot (2).png" alt="" />
+    <div className="mb-5 border border-gray-300 p-2 shadow-sm">
+      <img src={img} alt="" />
+      <div className="flex justify-around w-2/4 mx-auto mt-3 items-center text-2xl">
+        <FontAwesomeIcon onClick={() => downHandler(id)} icon={faCaretUp} size="xl" />
+        <FontAwesomeIcon onClick={() => upHandler(id)} icon={faCaretDown} size="xl" />
+        <FontAwesomeIcon icon={faTrash} size="sm" />
+      </div>
     </div>
   );
 }
